e689320b1e2ff9e487ec55ac8d75eadc109bec36
Alex <alex.wang3322@gmail.com>
2016-03-04 10:07:46 +0800

    update

--- StringUtils.java
+++ StringUtils.java
@@ -1,20 +1,61 @@
 class StringUtils{
 
-	public static void write(String content, File traget){
-	
-	}
-	public static void write(String content, File... tragets){
-		for(int i=0; i<tragets.length; i++){
-			write(content, tragets[i]);
-		}	
-	}
+    public static void write(String content, File traget){
+	BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedReader = new BufferedReader(new StringReader(content));
+            bufferedWriter = new BufferedWriter(new FileWriter(target));
+            char buf[] = new char[512];
+            int len;
+            while ((len = bufferedReader.read(buf)) != -1) {
+                bufferedWriter.write(buf, 0, len);
+            }
+            bufferedWriter.flush();
+            bufferedReader.close();
+            bufferedWriter.close();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }	
+    }
+    public static void write(String content, File... tragets){
+	for(int i=0; i<tragets.length; i++){
+		write(content, tragets[i]);
+	}	
+    }
 
-	public static String read(InputStream src){
-	
-	}
+    public static String read(InputStream src){
+	BufferedReader reader = new BufferedReader(new InputStreamReader(src));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                src.close();
+            } catch (IOException e) {
+            }
+        }
+        if(sb!=null && sb.length()>1){
+            return sb.toString();
+        }else{
+            return null;
+        }	
+    }
 
-	public static String read(File src){
-	
-	}
+    public static String read(File src){
+	FileInputStream is = new FileInputStream(src);
+        return read(is);	
+    }
 
 }
