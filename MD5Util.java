96f1f94dd649830c4af0d94f37eb299ed1a829b4
Alex <alex.wang3322@gmail.com>
2016-03-05 10:20:29 +0800

    update

--- MD5Util.java
+++ MD5Util.java
@@ -33,7 +33,7 @@ public class MD5Util {
 
     public static String md5Digest(InputStream paramInputStream)throws IOException {
         MessageDigest localMessageDigest = getDigest(MD5);
-        byte[] arrayOfByte = new byte[131072];
+        byte[] arrayOfByte = new byte[8192];
         for (int i = paramInputStream.read(arrayOfByte, 0, 131072);
                          i > -1;
                          i = paramInputStream.read(arrayOfByte, 0, 131072))
@@ -41,4 +41,43 @@ public class MD5Util {
         return getHexString(localMessageDigest.digest());
     }
 
+    public static String calculateMD5(File updateFile) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "Exception while getting digest", e);
+            return null;
+        }
+
+        InputStream is;
+        try {
+            is = new FileInputStream(updateFile);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "Exception while getting FileInputStream", e);
+            return null;
+        }
+
+        byte[] buffer = new byte[8192];
+        int read;
+        try {
+            while ((read = is.read(buffer)) > 0) {
+                digest.update(buffer, 0, read);
+            }
+            byte[] md5sum = digest.digest();
+            BigInteger bigInt = new BigInteger(1, md5sum);
+            String output = bigInt.toString(16);
+            // Fill to 32 chars
+            output = String.format("%32s", output).replace(' ', '0');
+            return output;
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to process file for MD5", e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Exception on closing MD5 input stream", e);
+            }
+        }
+    }
 }
