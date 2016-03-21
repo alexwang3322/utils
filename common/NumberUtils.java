2740d2d81114ad6c8a205b54b372120bbf0e3625
Alex <alex.wang3322@gmail.com>
2016-03-21 10:09:24 +0800

    update

---
+++ common/NumberUtils.java
@@ -0,0 +1,47 @@
+
+public final class NumberUtils {
+    private static final String TAG = "NumberUtils";
+
+    private NumberUtils() {
+    }
+
+    public static long tryParseLong(String s, long invalidLong) {
+        try {
+            invalidLong = Long.parseLong(s);
+        } catch (NumberFormatException e) {
+            Log.w(TAG, e);
+        }
+        return invalidLong;
+    }
+
+    public static double tryParseDouble(String s, double invalidDouble) {
+        try {
+            invalidDouble = Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            Log.w(TAG, e);
+        }
+        return invalidDouble;
+    }
+
+    public static double tryPercentageIntoDecimal(String s, double invalidDouble) {
+        try {
+            return Double.parseDouble(s) / 100.0d;
+        } catch (NumberFormatException e) {
+            Log.w(TAG, e);
+            return invalidDouble;
+        }
+    }
+
+    public static int tryParseInt(String s, int invalidInt) {
+        try {
+            invalidInt = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            Log.w(TAG, e);
+        }
+        return invalidInt;
+    }
+
+    public static String formatLatLong(double latOrLong) {
+        return String.format(Locale.US, "%.5f", new Object[]{Double.valueOf(latOrLong)});
+    }
+}
