f3439d0182f2e561ccb84e34ac74d46a9f5a71ad
Alex <alex.wang3322@gmail.com>
2016-05-04 11:57:21 +0800

    update

--- business/MyAppUtils.java
+++ business/MyAppUtils.java
@@ -6,15 +6,18 @@ public class MyAppUtils extends AppUtils{
     /**
      * 对登录数据进行操作
      * **/
-    public static void onLogin() {
+    public static void onLogin(Context context) {
 
     }
     /**
      * 对退出登录时的数据进行操作
      * **/
-    public static void onLogout() {
+    public static void onLogout(Context context) {
 
     }
 
+    public static void onLaunched(Context context) {
 
+    }
+ 
 }
